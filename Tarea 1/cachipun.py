9b814a6c23a3e922312729382596c14ea0a9689c
Daniel <satelerd@gmail.com>
2022-03-20 19:53:02 -0300

    better functions... (cmon codex)... needs more work, but no time

--- Tarea 1/cachipun.py
+++ Tarea 1/cachipun.py
@@ -2,31 +2,6 @@
 import random
 
 
-def start_game():
-    # Starts the game
-    print("Welcome to the paper rock scissors game")
-    print("The rules are simple:")
-    print("1. You will be asked to choose between rock, paper or scissors")
-    print("2. The computer will randomly choose between rock, paper or scissors")
-    print("3. The winner is the first one to reach 5 points")
-    print("4. Good luck!")
-    print()
-    print("Let's start the game!")
-    print()
-
-
-def get_user_choice():
-    # Asks the user to choose between rock, paper or scissors
-    user_choice = input("Please choose rock, paper or scissors: ")
-    return user_choice
-
-
-def get_computer_choice():
-    # Randomly chooses between rock, paper or scissors
-    computer_choice = random.choice(["rock", "paper", "scissors"])
-    return computer_choice
-
-
 def get_winner(user_choice, computer_choice):
     # Determines the winner
     if user_choice == computer_choice:
@@ -59,11 +34,24 @@ def play_again():
 
 def main():
     # The main function that runs the game
-    start_game()
+
+    # Starts the game
+    print("Welcome to the paper rock scissors game")
+    print("The rules are simple:")
+    print("1. You will be asked to choose between rock, paper or scissors")
+    print("2. The computer will randomly choose between rock, paper or scissors")
+    print("3. The winner is the first one to reach 5 points")
+    print("4. Good luck!")
+    print()
+    print("Let's start the game!")
+    print()
+
     user_score = 0
     computer_score = 0
     while True:
-        user_choice = get_user_choice()
+        # Asks the user to choose between rock, paper or scissors
+        user_choice = input("Please choose rock, paper or scissors: ")
+
         # if the user enters anything other than rock, paper or scissors, the game will ask again
         if (
             user_choice != "rock"
@@ -74,12 +62,16 @@ def main():
             print()
             continue
 
-        computer_choice = get_computer_choice()
+        # Asks the computer to randomly choose between rock, paper or scissors
+        computer_choice = random.choice(["rock", "paper", "scissors"])
         print(f"The computer chose {computer_choice}")
-        print(f"The result is: {get_winner(user_choice, computer_choice)}")
-        if get_winner(user_choice, computer_choice) == "You won!":
+
+        # Determines the winner
+        winner = get_winner(user_choice, computer_choice)
+        print(f"The result is: {winner}")
+        if winner == "You won!":
             user_score += 1
-        elif get_winner(user_choice, computer_choice) == "The computer won!":
+        elif winner == "The computer won!":
             computer_score += 1
 
         print()
@@ -87,6 +79,7 @@ def main():
         print(f"The computer's score is: {computer_score}")
         print()
 
+        # Checks if the game is over
         if user_score == 5:
             print("You won the game!")
             print()
