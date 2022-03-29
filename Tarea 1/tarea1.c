012333e311afdece7e07e7e0fac08ed5b48450e6
Daniel <satelerd@gmail.com>
2022-03-28 22:58:20 -0300

    menu update

--- Tarea 1/tarea1.c
+++ Tarea 1/tarea1.c
@@ -13,133 +13,124 @@ int read_csv()
     FILE *fp = fopen("file_path", "r");
 }
 
-// function to print the start menu
-int menu()
-{
-}
-
 // Function to add a new book
-int add_book(fp)
+int add_book()
 {
 }
 
 // Function to remove a book
-int remove_book(fp)
+int remove_book()
 {
 }
-
 // Function to add a campus
-int add_campus(fp)
+int add_campus()
 {
 }
 
 // Function to remove a campus (it should only be possible if the campus has no books)
-int remove_campus(fp)
+int remove_campus()
 {
 }
 
 // Function to edit a book
-int edit_book(fp)
+int edit_book()
 {
 }
 
 // Funtion to change a book from one campus to another
-int change_campus(fp)
+int change_campus()
 {
 }
 
 // Function to change a book from one section to another
-int change_section(fp)
+int change_section()
 {
 }
 
 // Function to change a book from one shelf to another
-int change_shelf(fp)
+int change_shelf()
 {
 }
 
 // Function to add a section
-int add_section(fp)
+int add_section()
 {
 }
 
 // Function to delete a section
-int delete_section(fp)
+int delete_section()
 {
 }
 
 // Function to add a shelf
-int add_shelf(fp)
+int add_shelf()
 {
 }
 
 // Function to delete a shelf (it should only be possible if the shelf has no books)
-int delete_shelf(fp)
+int delete_shelf()
 {
 }
 
 // Function to search for a book and print the information associated with it
-int search_book(fp)
+int search_book()
 {
 }
 
 // Function to close and save the .csv file
-int close_file(fp)
+int close_file()
 {
+}
+
+// function to print the start menu
+int menu()
+{
+    int num;
+    printf("\n");
+    printf("------------------------------------------------------\n");
+    printf("Bienvenido a la biblioteca de la UAI\n");
+    printf("Lo siguiente son las opciones para usar el sistema, escribe el numero de la opcion que deseas\n");
+    printf("1. Agregar libro\n");
+    printf("2. Eliminar libro\n");
+    printf("3. Agregar campus\n");
+    printf("4. Eliminar campus\n");
+    printf("5. Editar libro\n");
+    printf("6. Cambiar campus\n");
+    printf("7. Cambiar seccion\n");
+    printf("8. Cambiar estante\n");
+    printf("9. Agregar seccion\n");
+    printf("10. Eliminar seccion\n");
+    printf("11. Agregar estante\n");
+    printf("12. Eliminar estante\n");
+    printf("13. Buscar libro\n");
+    printf("14. Salir\n");
+    // get the input of the user and return it
+    printf("\n");
+    printf("Elige una opcion: ");
+    scanf("%d", &num);
+    printf("------------------------------------------------------\n");
+    printf("\n");
+
+    return num;
 }
 
 // the main function that calls the functions
 int main()
 {
     int option;
-    do
+    option = menu();
+
+    // switch to call the functions
+    switch (option)
     {
-        option = menu();
-        switch (option)
-        {
-        case 1:
-            add_book(fp);
-            break;
-        case 2:
-            remove_book(fp);
-            break;
-        case 3:
-            add_campus(fp);
-            break;
-        case 4:
-            remove_campus(fp);
-            break;
-        case 5:
-            edit_book(fp);
-            break;
-        case 6:
-            change_campus(fp);
-            break;
-        case 7:
-            change_section(fp);
-            break;
-        case 8:
-            change_shelf(fp);
-            break;
-        case 9:
-            add_section(fp);
-            break;
-        case 10:
-            delete_section(fp);
-            break;
-        case 11:
-            add_shelf(fp);
-            break;
-        case 12:
-            delete_shelf(fp);
-            break;
-        case 13:
-            search_book(fp);
-            break;
-        case 14:
-            close_file(fp);
-            break;
-        }
-    } while (option != 14);
+    case 1:
+        printf("Agregar libro\n");
+        add_book();
+        break;
+    case 2:
+        printf("Eliminar libro\n");
+        remove_book();
+        break;
+    }
     return 0;
 }
